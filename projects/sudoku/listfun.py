4990405de3f3940bf11130dc4b1c6f2e0a0a300e
Nícolas Dal Corso <rdalcorso.nicolas@gmail.com>
2024-10-27 19:08:15 -0300

    Update listfun.py
    implementation of list functions useful for sudoku solving

--- projects/sudoku/listfun.py
+++ projects/sudoku/listfun.py
@@ -28,30 +28,58 @@ def swap(iter: list, a: any, b: any) -> list:
     iter[a], iter[b] = iter[b], iter[a];
     return iter;
 
-def possibilities(iter: list, elem: any) -> list:
-    try:
-        index = iter.index(elem);
-    except ValueError:
-        return [];
+def sconstraints(iter: list, elem: any) -> list:
+    """Returns a new list with all elements of `iter` that are not equal to `elem`
     
-    if(index == 0):
-        return [index + 1];
-    elif(index == len(iter) - 1):
-        return [index - 1];
-    else:
-        return [index - 1, index + 1];
+    Args:
+        iter (list): The list to search in
+        elem (any): The element to remove from the list
     
-def choose(iter: list, elem: any) -> tuple[list, any]:
-    if(possibilities(iter, elem) == []):
-        return (iter, elem);
-    else:
-        return max(possibilities(iter, elem), key=lambda x: dist(iter, elem, x));
+    Returns:
+        list: The list with `elem` removed
+    """
+    temp = [x for x in iter if x != elem];
+    return sremove_empty(temp);
+
+def snum_constraints(iter: list, elem: any) -> int:
+    """Returns the number of elements in `iter` that are not equal to `elem`
+    
+    Args:
+        iter (list): The list to search in
+        elem (any): The element to remove from the list
+    
+    Returns:
+        int: The number of elements in `iter` that are not equal to `elem`
+    """
+    return len(sconstraints(iter, elem));
+
+def sremove(iter: list, elem: any) -> list:
+    """Returns a new list with all elements of `iter` that are equal to `elem`
+    
+    Args:
+        iter (list): The list to search in
+        elem (any): The element to remove from the list
+    
+    Returns:
+        list: The list with `elem` removed
+    """
+    temp = [x for x in iter if x == elem];
+    return sremove_empty(temp);
+
+def sremove_empty(iter: list) -> list:
+    """Returns a new list without all empty elements of `iter`
+    
+    Args:
+        iter (list): The list to search in
+    
+    Returns:
+        list: The list without empty elements
+    """
+    return [x for x in iter if x != 0];
 
 def main():
     l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    print(choose(l, 5));
-    
-    print(choose([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 0));
+    print(sconstraints(l, 5))
     
     
 if __name__ == "__main__":
